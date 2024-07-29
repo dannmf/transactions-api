95bf0ae8981d0d5011103176866bb972e2bd958f
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-29 20:21:37 -0300

    ✨feat: add zod to validate .env

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 const app = fastify()
 app.get('/hello', async () => {
   const tables = knex('sqlite_schema').select('*')
   return tables
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running!')
 })
