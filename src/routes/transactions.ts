dbcb820b387a685c8905172583cf5cab25053600
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-30 21:52:43 -0300

    ✨feat: create route post transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,26 @@
 import { FastifyInstance } from "fastify/types/instance"
 import { knex } from "../database"
+import { z } from 'zod'
+import {randomUUID} from 'crypto'
 
 export async function transactionRoutes(app: FastifyInstance) {
-    app.get('/hello', async () => {
-        const tables = knex('sqlite_schema').select('*')
-        return tables
+    app.post('/', async (request, reply) => {
+        const createTransactionBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit']),
+        })
+
+        const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+        await knex('transactions').insert({
+            id: randomUUID(),
+            title,
+            amount: type === 'credit' ? amount : amount * -1,
+            
+            
+        })
+
+        return reply.code(201).send()
     })
 }
