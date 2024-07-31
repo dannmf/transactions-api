bd353b2f36f29de552b25ef7f7408dc99e1470ce
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-30 22:10:24 -0300

    🐛fix: eslint an line sequence

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import fastify from 'fastify'
-import { knex } from './database'
+// import { knex } from './database'
 import { env } from './env'
 import { transactionRoutes } from './routes/transactions'
 const app = fastify()
@@ -7,7 +7,6 @@ const app = fastify()
 app.register(transactionRoutes, {
   prefix: 'transactions',
 })
-
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running!')
