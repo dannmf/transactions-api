6ba3895e64d8961f81331a9e0a1ca473911a0dbf
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-30 22:09:51 -0300

    🐛fix: incorrect table name in knex type file

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,14 +1,15 @@
-import { Knex } from 'knex';
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
+import { Knex } from 'knex'
 
 declare module 'knex/types/tables' {
-    export interface Tables {
-        transaction: {
-            id: string;
-            title: string;
-            amount: number;
-            created_at: string;
-            session_id?: string;
-        }
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id?: string
     }
+  }
 }
