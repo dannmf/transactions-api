417b3d2a7ed1dd8688a9a07ded7b54fc66bd4f90
daniel.manoel <daniellfmanoel@gmail.com>
2024-08-02 23:33:54 -0300

    ✨feat: add vitest and example test

---
+++ tests/example.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from 'vitest'
+
+test('o usuario consegue criar uma nova transação', () => {
+  const responseStatusCode = 201
+
+  expect(responseStatusCode).toEqual(201)
+})
